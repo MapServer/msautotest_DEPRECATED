b15a31a32d301e75811426e5dbc48e59d9b057ed
Jerome Villeneuve Larouche <jlarouche@mapgears.com>
2012-06-22 15:27:07 +0000

    Added labelLeaderObj tests

--- php/labelleaderObjTest.php
+++ php/labelleaderObjTest.php
@@ -14,10 +14,36 @@ class LabelleaderObjTest extends PHPUnit_Framework_TestCase
         $this->labelleader = $class->leader;
     }
 
-    public function testpmaxdistance()
+    public function test__getmaxdistance()
     {
-
+        $this->assertEquals(30, $this->labelleader->maxdistance);
     }
+
+    /**
+     * @expectedException           MapScriptException
+     * @expectedExceptionMessage    Property 'maxdistance' is read-only
+     */
+    public function test__setmaxdistance()
+    {
+        $this->labelleader->maxdistance = 35;
+    }
+
+    public function test__getgridstep()
+    {
+        $this->assertEquals(5, $this->labelleader->gridstep);
+    }
+
+    /**
+     * @expectedException           MapScriptException
+     * @expectedExceptionMessage    Property 'gridstep' is read-only
+     */
+    public function test__setgridstep()
+    {
+        $this->labelleader->gridstep = 35;
+    }
+
+    
+
 }
 
 ?>
