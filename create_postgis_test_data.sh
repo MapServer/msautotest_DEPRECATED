fd4888a80ebfb0e9fb9452445ba5ce7b587027fc
Thomas Bonfort <thomas.bonfort@gmail.com>
2012-09-20 13:41:55 +0200

    more postgis tests
    - tests for time support
    - put connection info in an include

--- create_postgis_test_data.sh
+++ create_postgis_test_data.sh
@@ -22,3 +22,79 @@ shp2pgsql -s 4326 -g the_geom wxs/data/pattern12.shp pattern12 | psql -d msautot
 shp2pgsql -s 4326 -g the_geom wxs/data/pattern13.shp pattern13 | psql -d msautotest -U postgres
 shp2pgsql -s 4326 -g the_geom wxs/data/pattern14.shp pattern14 | psql -d msautotest -U postgres
 shp2pgsql -s 4326 -g the_geom wxs/data/pattern15.shp pattern15 | psql -d msautotest -U postgres
+
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern1 RENAME COLUMN time to time_str;
+alter TABLE pattern1 ADD COLUMN time timestamp without time zone;
+update pattern1 set time = to_timestamp(time_str,'YYYYMMDD');
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern2 RENAME COLUMN time to time_str;
+alter TABLE pattern2 ADD COLUMN time timestamp without time zone;
+update pattern2 set time = to_timestamp(time_str,'YYYY-MM-DD\"T\"HH24:MI:SS\"Z\"');
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern3 RENAME COLUMN time to time_str;
+alter TABLE pattern3 ADD COLUMN time timestamp without time zone;
+update pattern3 set time = to_timestamp(time_str,'YYYY-MM-DD\"T\"HH24:MI:SS');
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern4 RENAME COLUMN time to time_str;
+alter TABLE pattern4 ADD COLUMN time timestamp without time zone;
+update pattern4 set time = to_timestamp(time_str,'YYYY-MM-DD HH24:MI:SS');
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern5 RENAME COLUMN time to time_str;
+alter TABLE pattern5 ADD COLUMN time timestamp without time zone;
+update pattern5 set time = to_timestamp(time_str,'YYYY-MM-DD\"T\"HH24:MI');
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern6 RENAME COLUMN time to time_str;
+alter TABLE pattern6 ADD COLUMN time timestamp without time zone;
+update pattern6 set time = to_timestamp(time_str,'YYYY-MM-DD HH24:MI');
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern7 RENAME COLUMN time to time_str;
+alter TABLE pattern7 ADD COLUMN time timestamp without time zone;
+update pattern7 set time = to_timestamp(time_str,'YYYY-MM-DD\"T\"HH24');
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern8 RENAME COLUMN time to time_str;
+alter TABLE pattern8 ADD COLUMN time timestamp without time zone;
+update pattern8 set time = to_timestamp(time_str,'YYYY-MM-DD HH24');
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern9 RENAME COLUMN time to time_str;
+alter TABLE pattern9 ADD COLUMN time timestamp without time zone;
+update pattern9 set time = to_timestamp(time_str,'YYYY-MM-DD');
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern10 RENAME COLUMN time to time_str;
+alter TABLE pattern10 ADD COLUMN time timestamp without time zone;
+update pattern10 set time = to_timestamp(time_str,'YYYY-MM');
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern11 RENAME COLUMN time to time_str;
+alter TABLE pattern11 ADD COLUMN time timestamp without time zone;
+update pattern11 set time = to_timestamp(time_str,'YYYY');
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern12 RENAME COLUMN time to time_str;
+alter TABLE pattern12 ADD COLUMN time time without time zone;
+update pattern12 set time = to_timestamp(time_str,'\"T\"HH24:MI:SSZ')::time;
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern13 RENAME COLUMN time to time_str;
+alter TABLE pattern13 ADD COLUMN time time without time zone;
+update pattern13 set time = to_timestamp(time_str,'THH24:MI:SS')::time;
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern14 RENAME COLUMN time to time_str;
+alter TABLE pattern14 ADD COLUMN time timestamp without time zone;
+update pattern14 set time = to_timestamp(time_str,'YYYY-MM-DD HH24:MI:SSZ');
+"
+psql -U postgres -d msautotest -c " 
+alter TABLE pattern15 RENAME COLUMN time to time_str;
+alter TABLE pattern15 ADD COLUMN time timestamp without time zone;
+update pattern15 set time = to_timestamp(time_str,'YYYY-MM-DDZ');
+"
